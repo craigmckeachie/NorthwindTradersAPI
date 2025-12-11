de3338eb817c2e5628d6a8e4674edd6edd7ab199
Craig McKeachie <craigdmckeachie@gmail.com>
2025-12-11 16:36:59 -0500

    products controller findById

--- src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductsController.java
@@ -2,8 +2,10 @@ package com.pluralsight.NorthwindTradersAPI.controllers;
 
 import com.pluralsight.NorthwindTradersAPI.data.ProductDao;
 import com.pluralsight.NorthwindTradersAPI.data.ProductDaoJdbc;
+import com.pluralsight.NorthwindTradersAPI.model.Category;
 import com.pluralsight.NorthwindTradersAPI.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +14,22 @@ import java.util.List;
 
 @RestController
 public class ProductsController {
-
-    @Autowired
     ProductDao productDao;
 
-
+    @Autowired
+    public ProductsController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
 
     @RequestMapping(path="products")
     public List<Product> getAll(){
         List<Product> products = productDao.getAll();
         return products;
+    }
 
+    @RequestMapping(path = "products/{id}")
+    public Product findById(@PathVariable int id){
+        return productDao.getById(id);
     }
 
 }
